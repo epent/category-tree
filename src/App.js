1f48e1277372b7c6acbc2f83fb30df37df9d4a43
epent <pentium95@mail.ru>
2022-01-03 11:17:38 +0200

    render with dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import React from "react";
+
+import Category from "./Category";
 
 function App() {
-  return <div className="App"></div>;
+  const data = {
+    value: "Cat 1",
+    children: [
+      {
+        value: "Cat 1-1",
+        children: [],
+      },
+      {
+        value: "Cat 1-2",
+        children: [
+          {
+            value: "Cat 1-2-1",
+            children: [],
+          },
+        ],
+      },
+    ],
+  };
+  return (
+    <div className="App">
+      <ul>
+        <Category data={data} />
+      </ul>
+    </div>
+  );
 }
 
 export default App;
